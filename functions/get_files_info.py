bbd11fabfc9031be75922adc75cade84e0d84fde
Amr <240349211+reconfirmok@users.noreply.github.com>
2026-01-07 12:39:16 +0200

    feat: get files

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs_path = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs_path, directory))
+        
+        if os.path.commonpath([working_dir_abs_path, target_dir]) == working_dir_abs_path:
+            if not os.path.isdir(target_dir):
+                return f'Error: "{target_dir}" is not a directory'
+            dir_items = []
+            try:
+                for item in os.listdir(target_dir):
+                    item_path = os.path.join(target_dir, item)
+                    dir_items.append(
+                        f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}"
+                        )
+                return "\n".join(dir_items)
+            except Exception as e:
+                return f"Error: {e}"
+        else:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    except Exception as e:
+        return f"Error listing files: {e}"
