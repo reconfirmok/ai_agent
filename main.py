044afddd7662e1d11806a5a20c60547feb1ecc87
Amr <240349211+reconfirmok@users.noreply.github.com>
2026-01-07 10:33:27 +0200

    feat: user input

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    parser = argparse.ArgumentParser(prog='Chatbot', description="ai agent using google gemini API")
+    parser.add_argument("user_prompt", type=str, help="User prompt to send to the model")
+    args = parser.parse_args()
+    user_prompt = args.user_prompt
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY is not set")
 
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=prompt
+        contents=user_prompt
     )
 
     if not response.usage_metadata:
         raise RuntimeError("Invalid response from Gemini API")
 
-    print(f"User prompt: {prompt}")
+    print(f"User prompt: {user_prompt}")
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
-    
+
     print("Response:")
     print(response.text)
 
