88da1560139175ead2c9cc79003075f7841405f5
Amr <240349211+reconfirmok@users.noreply.github.com>
2026-01-07 10:13:01 +0200

    token metadata

--- main.py
+++ main.py
@@ -11,11 +11,19 @@ def main():
         raise RuntimeError("GEMINI_API_KEY is not set")
 
     client = genai.Client(api_key=api_key)
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=prompt
     )
 
+    if not response.usage_metadata:
+        raise RuntimeError("Invalid response from Gemini API")
+
+    print(f"User prompt: {prompt}")
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
     print("Response:")
     print(response.text)
 
